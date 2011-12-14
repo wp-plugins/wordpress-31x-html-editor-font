fb4d4b5aefd35f564bb3ad7ff2614d9c749ed1cf
rockschtar <rockschtar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-14 08:36:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wordpress-31x-html-editor-font/trunk@475242 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html31xeditorfont.php
+++ html31xeditorfont.php
@@ -2,9 +2,9 @@
 /*
   Plugin Name: HTML 3.1x Editor Font
   Plugin URI: http://www.eracer.de/
-  Description: Brings the old HTML-Editor font from Wordpres 3.1x back to WordPress 3.2
+  Description: Brings the old HTML-Editor font from Wordpres 3.1x back to WordPress 3.2/3.3
   Author: Stevie
-  Version: 1.1
+  Version: 1.2
   Author URI: http://www.eracer.de
  */
 
